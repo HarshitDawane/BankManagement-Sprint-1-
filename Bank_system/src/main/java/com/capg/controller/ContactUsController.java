7ebb767ec77e5daf3aac023a4f0d06d304af595b
kalakhushi18 <khushikala18@gmail.com>
2022-05-06 12:35:37 +0530

    changes

--- Bank_system/src/main/java/com/capg/controller/ContactUsController.java
+++ Bank_system/src/main/java/com/capg/controller/ContactUsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.capg.Repository.ContactUsRepository;
 import com.capg.entity.ContactUs;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/contactUs")
 public class ContactUsController {
 
